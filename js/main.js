306d9292aac770b5bfceb0ff4405432bf1aaa66b
Humberto Neto <109989097+humberto-de-freitas-developer@users.noreply.github.com>
2022-08-17 22:45:39 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+
+const usuarios = "admin"
+const password = "12345";
+
+function check_login(){
+    const inputEmail = document.getElementsByName('usuario')[0].value;
+    const inputSenha = document.getElementsByName('senha')[0].value;
+ 
+    if(usuarios == inputEmail && password == inputSenha){
+        document.location.href = "login.html";
+
+    } else{
+        alert("Usuário ou senha errados");
+        
+    }
+        
+    
+
+}
+function exit (){
+    document.location.href="index.html";
+}
